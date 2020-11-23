05b0223f89df430ead6e77de578f51a7dfad3f62
Alex Merced <willcodeforfood@alexmerced.com>
2020-11-23 10:44:55 -0500

    first commit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
     cookie: { secure: process.env.NODE_ENV === "production" },
   })
 );
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //comment if not using forms
 // app.use(express.json()) uncomment if using json
 app.use(morgan("tiny")); //logging
@@ -41,37 +41,28 @@ app.use(morgan("tiny")); //logging
 //////////////
 
 expressVue.use(app).then(() => {
-
   app.get("/", (req, res) => {
-
     //Data to use in your vue
     const data = {
-      hello: "World"
-    }
+      hello: "World",
+    };
 
     //options to pass into route such as page title property
     const routeOptions = {
       head: {
-        title: "Vue Express View Engine"
-      }
-    }
+        title: "Vue Express View Engine",
+      },
+    };
 
     res.renderVue("index.vue", data, routeOptions);
   });
 
   app.get("/hello", (req, res) => {
-    res.send("hello")
-  })
-
+    res.send("hello");
+  });
 
   //LISTENER
   app.listen(PORT, () => {
-  console.log(`Your are listening on port ${PORT}`);
+    console.log(`Your are listening on port ${PORT}`);
+  });
 });
-
-});
-
-
-
-
-
